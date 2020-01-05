2e85009e2c4ae5f2683dc9ade0f49ed78580ddad
clinton231995 <55691136+clinton231995@users.noreply.github.com>
2020-01-05 02:13:55 +0000

    Create app.py

---
+++ Git2/app.py
@@ -0,0 +1,9 @@
+from myproject import app,db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, redirect, request, url_for, flash,abort
+from flask_login import login_user,login_required,logout_user
+#from myproject.models import User,products
+from myproject.forms import LoginForm, RegistrationForm,AddForm,delform
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import sessionmaker,relationship
